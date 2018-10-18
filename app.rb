41a7ac3271ed04249d55250869534382c223a85d
Ahmed <ahmed.marzook@cgi.com>
2018-10-18 12:55:05 +0000

    Fixing stuff

--- app.rb
+++ app.rb
@@ -3,28 +3,31 @@ require "date"
 
 set :session_secret, 'super secret'
 
-post '/' do
+get '/' do
   @name = params[:name]
   @bday = params[:bday]
   if params[:bday] != nil
     @date1 = Date.new(Date.today.year, Date.parse(@bday).month, Date.parse(@bday).day)
-    if @date1 = Date.new(12/04/2014)
+    if @date1 = Date.today
       redirect to("/happybday?name=#{@name}")
     elsif @date1 > Date.today
       @diff = @date1.mjd - Date.today.mjd
       redirect to("/countdown?name=#{@name}&diff=#{@diff}")
     else
       @diff = Date.new(Date.today.year + 1, @date1.month, @date1.day).mjd - Date.today.mjd
-      redirect to('/countdown'), @name, @diff
+      redirect to("/countdown?name=#{@name}&diff=#{@diff}")
     end
   end
   erb :index
 end
 
-get '/happybday' do
+post '/happybday' do
+  @name = params[:name]
   erb :hapbday
 end
 
-get '/countdown' do
+post '/countdown' do
+  @name = params[:name]
+  @diff = params[:diff]
   erb :countdown
 end
