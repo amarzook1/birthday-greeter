2bf54830a75b3b16837b0e24102667faf0a6d08b
Ahmed Marzook <m4marzook@gmail.com>
2018-11-01 10:31:09 +0000

    Birthday Greeter

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,17 +2,18 @@ require 'date'
 
 class Birthday
   def self.calculate(date1, name)
-    date1 = Date.today.year, Date.parse(date1).month, Date.parse(date1).day
+    date1 = Date.new(Date.today.year, Date.parse(date1).month, Date.parse(date1).day)
     date2 = Date.today
     name = name
     if date1 = date2
-      return "Happy birthday #{name}"
+      @result = "#{date1} #{date2} Happy birthday #{name}"
     elsif date1 > date2
       diff = date1.mjd - date2.mjd
-      return "#{diff} till your birthday #{name}"
+      @result = "#{diff} till your birthday #{name}"
     else
       diff = Date.new(Date.today.year + 1, date1.month, date1.day).mjd - date2.mjd
-      return "#{diff} till your birthday #{name}"
+      @result = "#{diff} till your birthday #{name}"
     end
+    return @result
   end
 end
